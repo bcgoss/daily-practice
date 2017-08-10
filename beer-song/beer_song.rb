9748f1401d6f7e27c28973f0342cc9d9cf9809b1
Bryan Goss <bcgoss@gmail.com>
2017-08-10 15:24:22 -0700

    Refactor pronoun and max bottles
    Allows beer song to be initialized with a maximum nuber of bottles
    and default to 99. Also made pronoun a method instead of a local
    variable, since a ternary operator and an assignment on the same
    line looks cluttered.

--- beer-song/beer_song.rb
+++ beer-song/beer_song.rb
@@ -1,5 +1,9 @@
 # Sings a song about alcoholism
 class BeerSong
+  def initialize(max_bottles = 99)
+    @max_bottles = max_bottles
+  end
+
   def verse(num)
     @number = num
     first_line + second_line
@@ -22,7 +26,7 @@ class BeerSong
 
   def quantity(number)
     if number == -1
-      '99 bottles'
+      quantity(@max_bottles)
     elsif number.zero?
       'no more bottles'
     elsif number == 1
@@ -40,9 +44,12 @@ class BeerSong
     if number.zero?
       'Go to the store and buy some more, '
     else
-      pronoun = number == 1 ? 'it' : 'one'
       "Take #{pronoun} down and pass it around, "
     end
+  end
+
+  def pronoun
+    number == 1 ? 'it' : 'one'
   end
 end
 
